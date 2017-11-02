aa1143d90057946686fdde52fe3db84230562c87
ozbo1973 <bbbove20@gmail.com>
2017-11-01 20:23:22 -0600

    Adding functionality to be able to email from web, only opens email program.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
 
 
+// ***listeners
+  $('#submit-email').click(function(e) {
+    e.preventDefault();
+  // information to be sent to the server
+    var info = $('#email-message').val();
+    console.log(info);
+    // $.ajax({
+    //     type: "POST",
+    //     url: 'server.php',
+    //     data: {msg: info}
+    // });
+    window.open('mailto:bbbove@gmail.com?subject=hire me&body='+info);
+    window.location.href='index.html';
+});
+
   $('#projects .img-thumbnail').hover(function(){
     $(this).css('cursor','pointer').css('background-color','black');
     $(this).attr('src',$(this).data('swap_img'));
